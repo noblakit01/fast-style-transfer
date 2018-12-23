46278ee1ce300d2eb4036976e7c830a6d930e25e
Luan Tran <noblakit01@gmail.com>
2018-12-23 23:45:06 +0700

    Add default argument for ckpt_to_mlmodel

--- ckpt_to_mlmodel.sh
+++ ckpt_to_mlmodel.sh
@@ -1,3 +1,6 @@
-python export_pb.py --checkpoint ../models/ckpt/$1.ckpt --in-path ../inputs/ --out-path ../outputs/ --device "/cpu:0" --batch-size 1
+PHOTO=${2:-chicago}
+python export_pb.py --checkpoint ../models/ckpt/$1.ckpt --in-path ../inputs/$PHOTO.jpg --out-path ../outputs/$PHOTO_$1.jpg --device "/cpu:0" --batch-size 1
 python pb_convert_mlmodel.py --pb-output $1
 python mlmodel_fix_output.py --mlmodel-output $1
+temp_file='../models/mlmodel/temp.mlmodel'
+[ -f $temp_file ] && rm $temp_file
