396f60f9ca37e1ff86259bd8796316847a3db7c7
Luan Tran <noblakit01@gmail.com>
2018-12-24 21:54:31 +0700

    Update output path

--- ckpt_to_mlmodel.sh
+++ ckpt_to_mlmodel.sh
@@ -1,5 +1,7 @@
 PHOTO=${2:-chicago}
-python export_pb.py --checkpoint ../style-transfer-models/ckpt/$1.ckpt --in-path ../inputs/$PHOTO.jpg --out-path ../outputs/$PHOTO_$1.jpg --device "/cpu:0" --batch-size 1
+OUTPUT="../outputs/$PHOTO"
+OUTPUT+="_$1.jpg"
+python export_pb.py --checkpoint ../style-transfer-models/ckpt/$1.ckpt --in-path "../inputs/$PHOTO.jpg" --out-path $OUTPUT --device "/cpu:0" --batch-size 1
 python pb_convert_mlmodel.py --pb-output $1
 python mlmodel_fix_output.py --mlmodel-output $1
 temp_file='../style-transfer-models/mlmodel/temp.mlmodel'
